1571103e6f6291d72369d3f8ae2ada812546f97e
Rami <ramilejmi@gmail.com>
2019-02-06 12:25:00 +0200

    Yritys2

--- application/auth/models.py
+++ application/auth/models.py
@@ -33,7 +33,7 @@ class User(Base):
     def find_users_with_no_tasks(:
         stmt = text("SELECT Account.id, Account.name FROM Account"
                      " LEFT JOIN Task ON Task.account_id = Account.id"
-                     " WHERE (Task.done IS null OR Task.done = :done)"
+                     " WHERE (Task.done IS null OR Task.done = True)"
                      " GROUP BY Account.id"
                      " HAVING COUNT(Task.id) = 0")
         res = db.engine.execute(stmt)
