b8324f401cac402cea0383b8d3f709eeb470d724
Rami <ramilejmi@gmail.com>
2019-03-01 20:28:17 +0200

    commenting back

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -8,9 +8,9 @@ class TaskForm(FlaskForm):
 
     name = StringField("Task name", [validators.Length(min=1), validators.Length(max=20)])
     done = BooleanField("Done")
-    groups_query = Groups.query.all()
-    lista = [(u.id, u.name) for u in groups_query]
-    groups = SelectField(u'Group', choices=lista)
+    # groups_query = Groups.query.all()
+    # lista = [(u.id, u.name) for u in groups_query]
+    # groups = SelectField(u'Group', choices=lista)
 
 
     class Meta:
